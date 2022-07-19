24a040f19bf3f7dcad5618456b28c13866a93dc8
Kresna Sucandra <kresnasucandra@gmail.com>
2022-07-19 22:22:26 +0800

    Expressons 10

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -12,4 +12,18 @@
 // * Use a match expression to determine which message
 //   to print
 
-fn main() {}
+fn main() {
+  let value = 999;
+
+  let is_gt_100 = value > 100;
+
+  print_message(is_gt_100);
+
+}
+
+fn print_message(gt_100: bool) {
+  match gt_100 {
+    true => println!("It's big"),
+    _ => println!("It's small"),
+  }
+}
