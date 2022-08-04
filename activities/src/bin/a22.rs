0076900a2695e7031baec10290349cdb9b26a661
Kresna Sucandra <kresnasucandra@gmail.com>
2022-08-04 08:23:34 +0800

    Testing a22

--- activities/src/bin/a22.rs
+++ activities/src/bin/a22.rs
@@ -23,7 +23,11 @@ fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
 
 /// Divides a and b.
 fn div(a: i32, b: i32) -> Option<i32> {
-    Some(a / b)
+    if b == 0 {
+        return None;
+    } else {
+        Some(a / b)
+    }
 }
 
 /// Takes two strings and places them immediately one after another.
@@ -32,3 +36,43 @@ fn concat(first: &str, second: &str) -> String {
 }
 
 fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn clamp_lower() {
+        let result = clamp(100, 10, 1000);
+        let expected = 100;
+        assert_eq!(result, expected, "should be 100");
+    }
+
+    #[test]
+    fn clamp_upper() {
+        let result = clamp(1000, 10, 10000);
+        let expected = 1000;
+        assert_eq!(result, expected, "should be 1000");
+    }
+
+    #[test]
+    fn check_div() {
+        let result = div(1, 1);
+        let expected = Some(1);
+        assert_eq!(result, expected, "should be 1");
+    }
+
+    #[test]
+    fn check_div_zero() {
+        let result = div(1, 0);
+        let expected = None;
+        assert_eq!(result, expected, "can not be divided by zero");
+    }
+
+    #[test]
+    fn check_concat() {
+        let result = concat("Hello", "world!");
+        let expected = "Hello world!".to_owned();
+        assert_eq!(result, expected, "should be Hello world!");
+    }
+}
