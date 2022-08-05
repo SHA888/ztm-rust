5a9ddc2d26a2342304e07d4f8b403321be3a285d
Kresna Sucandra <kresnasucandra@gmail.com>
2022-08-05 21:42:48 +0800

    External crates a26

--- activities/src/bin/a26.rs
+++ activities/src/bin/a26.rs
@@ -8,4 +8,11 @@
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
 
-fn main() {}
+use chrono::prelude::*;
+
+fn main() {
+  let utc: DateTime<Utc> = Utc::now();
+  let local: DateTime<Local> = Local::now();
+  println!("UTC: {:?}", utc.format("%Y-%m-%d %H:%M:%S").to_string());
+  println!("Local: {:?}", local.format("%Y-%m-%d %H:%M:%S").to_string());
+}
