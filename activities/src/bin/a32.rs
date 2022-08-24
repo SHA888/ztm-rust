93de50282e789e631bfa87a3fec217b573d697af
Kresna Sucandra <kresnasucandra@gmail.com>
2022-08-24 13:20:04 +0800

    Lifetimes and structures

--- activities/src/bin/a32.rs
+++ activities/src/bin/a32.rs
@@ -12,4 +12,36 @@
 
 const MOCK_DATA: &'static str = include_str!("mock-data.csv");
 
-fn main() {}
+struct Names<'a> {
+    inner: Vec<&'a str>
+}
+
+struct Titles<'a> {
+    inner: Vec<&'a str>
+}
+
+fn main() {
+    let data: Vec<_> = MOCK_DATA.split('\n').skip(1).collect();
+    let names: Vec<_> = data
+        .iter()
+        .filter_map(|line| line.split(',').nth(1))
+        .collect();
+    // for n in names.iter().take(3) {
+    //     println!("{}", n);
+    // }
+        let names = Names { inner: names };
+
+    let titles: Vec<_> = data
+        .iter()
+        .filter_map(|line| line.split(',').nth(4))
+        .collect();
+    // for n in titles.iter().take(3) {
+    //     println!("{}", n);
+    // }
+    let titles = Titles { inner: titles };
+
+    let data = names.inner.iter().zip(titles.inner.iter());
+    for (name, title) in data.take(5) {
+        println!("Name: {}, title: {}", name, title);
+    }
+}
