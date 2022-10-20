26f7f9c1d8c1400e3136456aed87e00727a0dc8d
Kresna Sucandra <kresnasucandra@gmail.com>
2022-10-20 11:51:40 +0800

    database type aliases

--- clipstash/src/lib/data/mod.rs
+++ clipstash/src/lib/data/mod.rs
@@ -3,6 +3,19 @@ use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 use uuid::Uuid;
 
+#[derive(Debug, thiserror::Error)]
+pub enum DataError {
+    #[error("database error: {0}")]
+    Database(#[from] sqlx::Error),
+}
+
+pub type AppDatabase = Database<Sqlite>;
+pub type DatabasePool = sqlx::sqlite::SqlitePool;
+pub type Transaction<'t> = sqlx::Transaction<'t, Sqlite>;
+pub type AppDatabaseRow = sqlx::sqlite::SqliteRow;
+pub type AppQueryResult = sqlx::sqlite::SqliteQueryResult;
+
+
 #[derive(Clone, Debug, Deserialize, Display, From, Serialize)]
 pub struct DbId(Uuid);
 
