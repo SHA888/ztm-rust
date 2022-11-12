357ca55eefbe061df68e8ddfdc898d4cc1dd0928
Kresna Sucandra <kresnasucandra@gmail.com>
2022-11-12 20:20:57 +0800

    hit counter part 2

--- clipstash/src/lib/domain/clip/field/shortcode.rs
+++ clipstash/src/lib/domain/clip/field/shortcode.rs
@@ -4,7 +4,9 @@ use rocket::{UriDisplayPath, UriDisplayQuery};
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
-#[derive(Clone, Debug, Deserialize, Serialize, From, UriDisplayPath, UriDisplayQuery, Hash, Eq, PartialEq)]
+#[derive(
+    Clone, Debug, Deserialize, Serialize, From, UriDisplayPath, UriDisplayQuery, Hash, Eq, PartialEq,
+)]
 pub struct ShortCode(String);
 
 impl ShortCode {
